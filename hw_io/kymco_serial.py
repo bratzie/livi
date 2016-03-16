3ec9fe3f9479d457ef97702ce1af1fa69ee41d98
chrand1 <christer.andreasson@alten.se>
2016-03-16 18:27:52 +0000

    fixed hex values

--- hw_io/kymco_serial.py
+++ hw_io/kymco_serial.py
@@ -7,36 +7,36 @@ import serial
 DEVICE = '/dev/ttyACMO' 
 
 
-class MC_Io_Serial:
+class MCIoSer:
 
 
     ON = 1
     OFF = 0
     
     TIMEOUT_SEC = 0.1
-    ALL_SIG = 0x19
-    ALLON = 0x64
-    ALLOFF = 0x6E
+    ALL_SIG = '\x19'
+    ALLON = '\x64'
+    ALLOFF = '\x6E'
 
-    input_Signals ={'IND_L' : 0x0,
-                    'IND_R' : 0x1,
-                    'HORN' : 0x2,
-                    'MAIN' : 0x3,
-                    'POS_L' : 0x4,
-                    'ENG' :  0x5,
-                    'HI_B' : 0x6,
-                    'LO_B' : 0x7,
+    input_Signals ={'IND_L' : '\x0',
+                    'IND_R' : '\x1',
+                    'HORN' : '\x2',
+                    'MAIN' : '\x3',
+                    'POS_L' :'\x4',
+                    'ENG' :  '\x5',
+                    'HI_B' : '\x6',
+                    'LO_B' : '\x7',
                     
     }
 
-    output_Signals ={'IND_L' : 0x65,
-                     'IND_R' : 0x66,
-                     'HORN' : 0x67,
-                     'MAIN' : 0x68,
-                     'POS_L' : 0x69,
-                     'ENG' :  0x6A,
-                     'HI_B' : 0x6B,
-                     'LO_B' : 0x6C,
+    output_Signals ={'IND_L' : '\x65',
+                     'IND_R' : '\x66',
+                     'HORN' : '\x67',
+                     'MAIN' : '\x68',
+                     'POS_L' : '\x69',
+                     'ENG' :  '\x6A',
+                     'HI_B' : '\x6B',
+                     'LO_B' : '\x6C',
                     
     }
 
@@ -160,7 +160,7 @@ class MC_Io_Serial:
 
 
 def main():
-    test_Serial = MC_Io_Serial()
+    test_Serial = MCIoSer()
     test_Serial.setup_Serial(DEVICE)
     
 if __name__ == "__main__":
